c36b5e40af5056708897ce16abb15aa6217b10d6
love8279 <lovekushchoudhary315@gmail.com>
2026-03-04 00:21:14 +0530

    Add configuration for API credentials and bot token

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "12345")) # Aapka API ID
+API_HASH = os.environ.get("API_HASH", "your_hash")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_token")
