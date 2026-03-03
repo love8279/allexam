0f3bfb2c18deb5beb0dc15481db73d445404d1f7
love8279 <lovekushchoudhary315@gmail.com>
2026-03-04 00:27:56 +0530

    Change API_ID, API_HASH, and BOT_TOKEN values
    Updated API credentials with new values.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", "12345")) # Aapka API ID
-API_HASH = os.environ.get("API_HASH", "your_hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_token")
+API_ID = int(os.environ.get("API_ID", "20417561")) # Aapka API ID
+API_HASH = os.environ.get("API_HASH", "c01124688b006ef00fa6ac926d6cb3e9")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8042172383:AAFeoSOWS10DQI-oJwY3O3x9ur_TxMEQq8g")
